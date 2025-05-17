3861ba16a5dbe5738f1696099a30c3b4fd158b4f
Jonas Albrecht <jonas.albrecht@zalando.de>
2025-05-17 17:45:57 +0200

    update sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -9,14 +9,31 @@ const pages = await globby.globby([
   '!src/pages/**/[*.js',
 ]);
 
+const components = await globby.globby([
+  'src/components/**/*.tsx',
+  'src/components/**/*.js',
+  '!src/components/_*.js',
+  '!src/components/**/[*.js',
+]);
+
 const sitemap = new SitemapStream({ hostname: 'https://albrecht-indutherm.com' });
 
+// Add pages as before
 pages.forEach(page => {
   let route = page
     .replace(/^src\/pages/, '')
     .replace(/(\/index)?\.(tsx|js)$/, '') || '/';
   if (route === '') route = '/';
   sitemap.write({ url: route, changefreq: 'weekly', priority: 0.7 });
+});
+
+// Add components as lowercase anchor links on the homepage
+components.forEach(component => {
+  const match = component.match(/src\/components\/(.+?)\.(tsx|js)$/);
+  if (match) {
+    const anchor = match[1].toLowerCase();
+    sitemap.write({ url: `/#${anchor}`, changefreq: 'weekly', priority: 0.7 });
+  }
 });
 
 sitemap.end();
